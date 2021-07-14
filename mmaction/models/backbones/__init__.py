23d1660881f2276f3450dd8ef36ceea2a6c05e06
SCZwangxiao <scz.wangxiao@gmail.com>
2021-07-14 23:40:47 +0800

    add tdn code

--- mmaction/models/backbones/__init__.py
+++ mmaction/models/backbones/__init__.py
@@ -8,6 +8,7 @@ from .resnet3d_csn import ResNet3dCSN
 from .resnet3d_slowfast import ResNet3dSlowFast
 from .resnet3d_slowonly import ResNet3dSlowOnly
 from .resnet_audio import ResNetAudio
+from .resnet_tdn import ResNetTDN
 from .resnet_tin import ResNetTIN
 from .resnet_tsm import ResNetTSM
 from .tanet import TANet
@@ -18,5 +19,5 @@ __all__ = [
     'C3D', 'ResNet', 'ResNet3d', 'ResNetTSM', 'ResNet2Plus1d',
     'ResNet3dSlowFast', 'ResNet3dSlowOnly', 'ResNet3dCSN', 'ResNetTIN', 'X3D',
     'ResNetAudio', 'ResNet3dLayer', 'MobileNetV2TSM', 'MobileNetV2', 'TANet',
-    'TimeSformer'
+    'ResNetTDN', 'TimeSformer'
 ]
